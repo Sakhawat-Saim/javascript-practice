ed6669eae4d8163a1a1d4285c9065a172e1dc826
Sakhawat-Saim <sakhawat.muhammod@gmail.com>
2023-01-31 01:28:30 +0600

    second commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// function startFun() {
+//     console.log('standup');
+//     console.log('walk towards the switch');
+//     console.log('press the switch');
+// }
+
+// call function
+// startFun();
+// startFun();
+// startFun();
+
+// parameter
+
+function bringSingara(money) {
+    console.log('taka dsen: ', money);
+}
+
+bringSingara(100);
+
+function sum(a, b, c, d, e) {
+    console.log(a,b,c,d,e);
+    const result = a + b + c + d  + e; 
+    return result;
+}
+
+x = sum(3,4,5,6,7);
+
+console.log('Total summation is: ', x);
+
